573f0cff0517a4ff63e2f8c535478da6177f11c5
劉謹賢 <liujinxian@liujinxiande-MacBook-Pro.local>
2020-07-10 06:09:17 +0800

    Add express-session

--- api/index.js
+++ api/index.js
@@ -3,6 +3,17 @@ const express = require('express')
 // Create express instance
 const app = express()
 
+// Add some modules
+var session = require('express-session');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+  secret : 'keyboard cat',
+  resave : true,
+  saveUninitialized:true
+}))
+
 // Require API routes
 const indexRouter = require('./routes/index');
 const users = require('./routes/users')
--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
     ],
     script: [
-      { src: 'https://code.jquery.com/jquery-3.5.1.slim.min.jss' },
+      { src: 'https://code.jquery.com/jquery-3.5.1.slim.min.js' },
       { src: 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js' },
       { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js' }
     ],
