6c782ded0653b373472146ad487435fac8da9e79
劉謹賢 <liujinxian@liujinxiande-MacBook-Pro.local>
2020-07-11 05:58:40 +0800

    Finish Navbar setting and error page.

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,20 +1,26 @@
 <template>
   <section class="container">
-    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
-    <h1 class="title">
-      {{ error.statusCode }}
-    </h1>
-    <h2 class="info">
-      {{ error.message }}
-    </h2>
-    <nuxt-link class="button" to="/" v-if="error.statusCode === 404">
-      Homepage
-    </nuxt-link>
+    <md-empty-state
+      md-rounded
+      md-icon="bubble_chart"
+      md-label="頁面不存在"
+      md-description="我們找不到您在尋找的頁面。">
+    </md-empty-state>
+    <div class="d-flex justify-content-center mt-3">
+      <md-button class="md-raised md-accent" @click="goHome()">回到首頁</md-button>
+    </div>
   </section>
 </template>
 <script>
 export default {
-  props: ['error']
+  props: ['error'],
+  methods: {
+    goHome(){
+      this.$router.push({
+        path: '/'
+      })
+    }
+  }
 }
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,14 @@
 <template>
     <div>
-        <Navbar />
+
     </div>
 </template>
 
 <script>
 import axios from '~/plugins/axios'
-import Navbar from '~/components/Navbar.vue'
 
 export default {    
   components: {
-    Navbar,
   },
   async asyncData () {
     //let { data } = await axios.get('/api/users')
