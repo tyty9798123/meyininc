015679f5b1a37d02f9347058e3c8bed3591ee37c
劉謹賢 <liujinxian@liujinxiande-MacBook-Pro.local>
2020-08-04 02:00:35 +0800

    Update Index.js add self-introduct page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,29 @@
 <template>
     <div>
+      <div class="text-center title">
+        <b>
+          <font size="6">B</font>
+          <font size="4">ERKSHIRE </font>
+          <font size="6">H</font>
+          <font size=4>ATHAWAY </font>
+          <font size="2">INC.<br>3555 Farnam Street<br>Omaha, NE 68131</font>
+          <br>Official Home Page
+        </b>
+      </div>
 
+      <hr>
+      <div class="row">
+      <div class="col-12 col-md-6">
+        <md-toolbar>
+          <h3 class="md-title">
+            <nuxt-link to='/author_introduction'>
+            <img src="~/assets/img/info_introduce.png" width="40"> 
+              作者自我介紹
+            </nuxt-link>
+          </h3>
+        </md-toolbar>
+      </div>
+      </div>
     </div>
 </template>
 
@@ -23,5 +46,7 @@ export default {
 </script>
 
 <style scoped>
-
+  .title{
+    color: #000080;
+  }
 </style>
