76789638788efa9343e7138349030e10fb2c827d
劉謹賢 <liujinxian@liujinxiande-MacBook-Pro.local>
2020-08-10 20:46:12 +0800

    Add reminding.vue

---
+++ pages/reminding.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <md-card md-with-hover>
+        <md-ripple>
+            <md-card-header>
+            <div class="md-title">提醒事項</div>
+            <div class="md-subhead">Last Updated At: 2020/01/24 18:00:00</div>
+            </md-card-header>
+
+            <md-card-content>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+            </md-card-content>
+
+            <md-card-actions>
+            <md-button>更新</md-button>
+            <md-button>完成</md-button>
+            </md-card-actions>
+        </md-ripple>
+    </md-card>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
